f03a44f64362cae04d43755e2bd821207f69e65e
stanislav.savolainen <stanislav.savolainen@mail.com>
2017-09-01 12:37:10 +0300

    redux-api and hapi-server test 1

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ React and redux test projects 1
 p4, dispatch action and update material-ui component, test 1 (redux with material-ui) 
 
 p7, use redux-act and react-redux library with standart redux, also modify .babelrc to support spread operator
+
+p9, redux-api and hapi-server test, http-request only at the moment done (first working test on my framework)
---
+++ p9/back_end/hapi.js
@@ -0,0 +1,35 @@
+'use strict';
+
+//NodeJS Hapi - module   ( Hapi / static-express )
+
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+
+server.connection({ host: 'localhost' , port: 5659, routes: {
+        cors: true
+      } });
+
+//use cors, because http-request come from other doimain than server
+
+
+// Add the route
+server.route({
+    method: 'POST',
+    path:'/link1', 
+    handler: function (request, reply) {
+
+	console.log("/hello POST-handler executed.");
+
+        //return reply('hello world');
+	return reply(JSON.stringify('hello world'));
+    }
+});
+
+// Start the server
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
